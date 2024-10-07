dd453dbbc05cd88d765393e01dbda3faebabe7f0
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-10-07 12:04:59 +0600

    Design Home layout &main Layout
    12:04PM
    07/10/2024

---
+++ AndroidStudioProject59/app/src/main/java/com/neyamul/androidStudioProject59/MainActivity.java
@@ -0,0 +1,40 @@
+package com.neyamul.androidStudioProject59;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+  private Button btnHome;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnHome=findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
