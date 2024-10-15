a4f64e36d131641e9e616dc057d1e5f2de64a563
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-10-16 00:40:32 +0600

    SqliteDb
    13:39Am
    16/10/2024

--- Project/Healthify/app/src/main/java/com/labs/healthify/Database.java
+++ Project/Healthify/app/src/main/java/com/labs/healthify/Database.java
@@ -11,9 +11,155 @@ import java.util.ArrayList;
 
 public class Database extends SQLiteOpenHelper{
 
+//    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+//        super(context, name, factory, version);
+//    }
+//
+//    @Override
+//    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+//        String qry1 = "create table users(username text , email text , password text)";
+//        sqLiteDatabase.execSQL(qry1);
+//
+//        String qry2 = "create table cart(username text,product text,price float,otype text )";
+//        sqLiteDatabase.execSQL(qry2);
+//
+//        String qry3 = "create table orderplace(username text,name text,address text,connum text , pin int , date text , time text , amount float,otype text)";
+//        sqLiteDatabase.execSQL(qry3);
+//    }
+//
+//    @Override
+//    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+//
+//    }
+//    public void register(String username , String email , String password){
+//        ContentValues cv = new ContentValues();
+//        cv.put("username",username);
+//        cv.put("email",email);
+//        cv.put("password",password);
+//        SQLiteDatabase db = getWritableDatabase();
+//        db.insert("users",null,cv);
+//        db.close();
+//    }
+//    public int login(String username , String password){
+//        int result=0;
+//        String str[]=new String[2];
+//        str[0]=username;
+//        str[1]=password;
+//        SQLiteDatabase db = getReadableDatabase();
+//        Cursor c = db.rawQuery("select * from users where username=? and password=?",str);
+//        if(c.moveToFirst()){
+//            result=1;
+//        }
+//        return result;
+//    }
+//
+//    public void addCart(String username , String product , float price , String otype){
+//        ContentValues cv = new ContentValues();
+//        cv.put("username",username);
+//        cv.put("product",product);
+//        cv.put("price",price);
+//        cv.put("otype",otype);
+//        SQLiteDatabase db = getWritableDatabase();
+//        db.insert("cart",null,cv);
+//        db.close();
+//    }
+//    public int checkCart(String username,String product){
+//        int result=0;
+//        String str[] = new String[2];
+//        str[0]=username;
+//        str[1]=product;
+//        SQLiteDatabase db = getReadableDatabase();
+//        Cursor c = db.rawQuery("select * from cart where username=? and product=?",str);
+//        if (c.moveToFirst()){
+//            result=1;
+//        }
+//        db.close();
+//        return result;
+//    }
+//
+//    public void removeCart(String username , String otype){
+//        String str[]=new String[2];
+//        str[0]=username;
+//        str[1]=otype;
+//        SQLiteDatabase db = getWritableDatabase();
+//        db.delete("cart","username=? and otype=?",str);
+//        db.close();
+//    }
+//
+//    public void addOrder(String username , String name , String address , String connum ,int pin , String date , String time , float price , String otype ){
+//        ContentValues cv = new ContentValues();
+//        cv.put("username",username);
+//        cv.put("name",name);
+//        cv.put("address",address);
+//        cv.put("connum",connum);
+//        cv.put("pin",pin);
+//        cv.put("date",date);
+//        cv.put("time",time);
+//        cv.put("amount",price);
+//        cv.put("otype",otype);
+//        SQLiteDatabase db = getWritableDatabase();
+//        db.insert("orderplace",null,cv);
+//        db.close();
+//    }
+//
+//    public ArrayList getCartData(String username , String otype){
+//        ArrayList<String> arr = new ArrayList<>();
+//        SQLiteDatabase db = getReadableDatabase();
+//        String str[] = new String[2];
+//        str[0] = username;
+//        str[1] = otype;
+//        Cursor c = db.rawQuery("select * from cart where username=? and otype=?",str);
+//        if (c.moveToFirst()){
+//            do {
+//                String product = c.getString(1);
+//                String price = c.getString(2);
+//                arr.add(product+"$"+price);
+//            }while (c.moveToNext());
+//        }
+//        db.close();
+//        return arr;
+//    }
+//
+//    public ArrayList getOrderData(String username){
+//        ArrayList<String> arr = new ArrayList<>();
+//        SQLiteDatabase db = getReadableDatabase();
+//        String str[] = new String[1];
+//        str[0]=username;
+//        Cursor c = db.rawQuery("select * from orderplace where username = ?",str);
+//        if (c.moveToFirst()){
+//            do {
+//                arr.add(c.getString(1)+"$"+c.getString(2)+"$"+c.getString(3)+"$"+c.getString(4)+"$"+c.getString(5)+"$"+c.getString(6)+"$"+c.getString(7)+"$"+c.getString(8));
+//            }while (c.moveToNext());
+//        }
+//        db.close();
+//        return arr;
+//    }
+//
+//    public int checkappontmentExists(String username , String ed1 , String ed2 , String ed3 , String date , String time){
+//        int result = 0;
+//        String str[]=new String[6];
+//        str[0]=username;
+//        str[1]=ed1;
+//        str[2]=ed2;
+//        str[3]=ed3;
+//        str[4]=date;
+//        str[5]=time;
+//        SQLiteDatabase db = getReadableDatabase();
+//        Cursor c =db.rawQuery("select * from orderplace where username=? and ed1=? and ed2=? and ed3=? and date=? and time=?",str);
+//        if (c.moveToFirst()){
+//            result=1;
+//        }
+//        db.close();
+//        return result;
+//    }
+public static final String DATABASE_NAME = "healthify.db";
+    public static final int DATABASE_VERSION = 1;
+
     public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
+
+
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
@@ -25,12 +171,14 @@ public class Database extends SQLiteOpenHelper{
 
         String qry3 = "create table orderplace(username text,name text,address text,connum text , pin int , date text , time text , amount float,otype text)";
         sqLiteDatabase.execSQL(qry3);
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
 
     }
+
     public void register(String username , String email , String password){
         ContentValues cv = new ContentValues();
         cv.put("username",username);
@@ -40,16 +188,19 @@ public class Database extends SQLiteOpenHelper{
         db.insert("users",null,cv);
         db.close();
     }
+
+
+
+
     public int login(String username , String password){
         int result=0;
-        String str[]=new String[2];
-        str[0]=username;
-        str[1]=password;
+        String[] str =new String[]{username, password};
         SQLiteDatabase db = getReadableDatabase();
         Cursor c = db.rawQuery("select * from users where username=? and password=?",str);
         if(c.moveToFirst()){
             result=1;
         }
+        c.close();
         return result;
     }
 
@@ -63,24 +214,22 @@ public class Database extends SQLiteOpenHelper{
         db.insert("cart",null,cv);
         db.close();
     }
+
     public int checkCart(String username,String product){
         int result=0;
-        String str[] = new String[2];
-        str[0]=username;
-        str[1]=product;
+        String[] str = new String[]{username,product};
         SQLiteDatabase db = getReadableDatabase();
         Cursor c = db.rawQuery("select * from cart where username=? and product=?",str);
         if (c.moveToFirst()){
             result=1;
         }
-        db.close();
+        c.close();
         return result;
     }
 
+
     public void removeCart(String username , String otype){
-        String str[]=new String[2];
-        str[0]=username;
-        str[1]=otype;
+        String[] str =new String[]{username,otype};
         SQLiteDatabase db = getWritableDatabase();
         db.delete("cart","username=? and otype=?",str);
         db.close();
@@ -102,12 +251,10 @@ public class Database extends SQLiteOpenHelper{
         db.close();
     }
 
-    public ArrayList getCartData(String username , String otype){
+    public ArrayList<String> getCartData(String username , String otype){
         ArrayList<String> arr = new ArrayList<>();
         SQLiteDatabase db = getReadableDatabase();
-        String str[] = new String[2];
-        str[0] = username;
-        str[1] = otype;
+        String[] str = new String[]{username,otype};
         Cursor c = db.rawQuery("select * from cart where username=? and otype=?",str);
         if (c.moveToFirst()){
             do {
@@ -116,40 +263,52 @@ public class Database extends SQLiteOpenHelper{
                 arr.add(product+"$"+price);
             }while (c.moveToNext());
         }
-        db.close();
+        c.close();
         return arr;
     }
 
-    public ArrayList getOrderData(String username){
+
+
+
+    public ArrayList<String> getOrderData(String username){
         ArrayList<String> arr = new ArrayList<>();
         SQLiteDatabase db = getReadableDatabase();
-        String str[] = new String[1];
-        str[0]=username;
+        String[] str = new String[]{username};
         Cursor c = db.rawQuery("select * from orderplace where username = ?",str);
         if (c.moveToFirst()){
             do {
                 arr.add(c.getString(1)+"$"+c.getString(2)+"$"+c.getString(3)+"$"+c.getString(4)+"$"+c.getString(5)+"$"+c.getString(6)+"$"+c.getString(7)+"$"+c.getString(8));
             }while (c.moveToNext());
         }
-        db.close();
+        c.close();
         return arr;
     }
 
+
+
+
     public int checkappontmentExists(String username , String ed1 , String ed2 , String ed3 , String date , String time){
         int result = 0;
-        String str[]=new String[6];
-        str[0]=username;
-        str[1]=ed1;
-        str[2]=ed2;
-        str[3]=ed3;
-        str[4]=date;
-        str[5]=time;
+        String[] str =new String[]{username,ed1,ed2,ed3,date,time};
+
         SQLiteDatabase db = getReadableDatabase();
         Cursor c =db.rawQuery("select * from orderplace where username=? and ed1=? and ed2=? and ed3=? and date=? and time=?",str);
         if (c.moveToFirst()){
             result=1;
         }
-        db.close();
+        c.close();
         return result;
     }
+
+    public User getUser(String username) {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = db.rawQuery("select * from users where username=?", new String[]{username});
+        User user = null;
+        if (c.moveToFirst()) {
+            user = new User(c.getString(0), c.getString(1), c.getString(2));
+        }
+        c.close();
+        return user;
+    }
+
 }
